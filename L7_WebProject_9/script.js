78694a25553d2af1a748b19eacdfcec3de1d154f
Sean <74019460+hawaiiankid@users.noreply.github.com>
2021-03-16 11:08:42 -0700

    Add files via upload

---
+++ L7_WebProject_9/script.js
@@ -0,0 +1,8 @@
+function nameInfo(){
+  var firstName = document.getElementById("firstName").value;
+  var middleName = document.getElementById("middleName").value;
+  var lastName = document.getElementById("lastName").value;
+  var fullName = firstName+""+middleName+""+lastName;
+
+document.getElementById("fullName").innerHTML = fullName;
+}
